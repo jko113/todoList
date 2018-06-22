766981e1d7ebbeb6d63812d19c53f4295bffce05
Joshua Owens <jko113@gmail.com>
2018-06-22 14:41:40 -0400

    exporting functions in db.js

--- db.js
+++ db.js
@@ -8,20 +8,52 @@ const cn = {
 }
 const db = pgp(cn);
 
-function getTodo(id) {
-    db.any('SELECT * FROM Todos WHERE Id = $1;', [id])
-        .then(function(data) {
-            // success;
-            console.log(data);
-        })
-        .catch(function(error) {
-            // error;
-            console.error(error);
-        });
+function getOne(id) {
+    return db.oneOrNone('SELECT * FROM Todos WHERE Id = $1;', [id]);
 }
 
-// getTodo(7);
+function getAll() {
+    return db.any('SELECT * FROM Todos;');
+}
+
+function getPending() {
+    return db.any('SELECT * FROM Todos \
+    WHERE IsDone = FALSE');
+}
+
+function getFinished() {
+    return db.any('SELECT * FROM Todos \
+    WHERE IsDone = TRUE');
+}
+
+function searchByTitle(searchString) {
+    return db.any('SELECT * FROM Todos WHERE Title LIKE \'%$1#%\'', [searchString]);
+}
+
+function deleteById(id) {
+    return db.result('DELETE FROM Todos WHERE Id=$1', [id]);
+}
+
+function setFinished(id, isDone) {
+    return db.result("UPDATE Todos SET IsDone = $1 WHERE Id = $2", [isDone, id]);
+}
+
+function setTitle(id, newTitle) {
+    return db.result("UPDATE Todos SET Title = \'$1#\' WHERE Id = $2", [newTitle, id]);
+}
+
+function add(title) {
+    return db.one("INSERT INTO Todos (Title, IsDone) VALUES (\'$1#\', false) RETURNING ID", [title]);
+}
 
 module.exports = {
-    getTodo: getTodo
+    getOne: getOne,
+    getAll: getAll,
+    getPending: getPending,
+    getFinished: getFinished,
+    searchByTitle: searchByTitle,
+    deleteById: deleteById,
+    setFinished: setFinished,
+    setTitle: setTitle,
+    add: add
 };
