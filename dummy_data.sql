ef02a28384735239a883a9bb5d28fded2f9da5cc
Joshua Owens <jko113@gmail.com>
2018-06-22 10:13:41 -0400

    adding dummy data file

---
+++ dummy_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO Todos (Title, IsDone)
+VALUES
+('Go to work',false),
+('Brush my teeth',false),
+('Bathe the cat',false),
+('Mow the lawn',false),
+('Trim the bushes',false),
+('Barbeque the zucchini',false),
+('Melt the cheese',false),
+('Eat some dinner',false);
