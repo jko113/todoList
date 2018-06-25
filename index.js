67b51b753935ab9088e862231a21e3aac0738bf8
Joshua Owens <jko113@gmail.com>
2018-06-25 11:23:07 -0400

    adding public views

--- index.js
+++ index.js
@@ -2,17 +2,35 @@ const express = require("express");
 const app = express();
 const Todo = require("./db");
 const port = 4321;
+const hbs = require("express-handlebars");
+const static = express.static;
+
+app.engine(".hbs", hbs({
+    defaultLayout: "layout",
+    extname: ".hbs"
+}));
+app.set("view engine", ".hbs");
+app.use(static("public"));
 
 app.get("/", (req, res) => {
     Todo.getAll()
-        .then(data => res.send(data))
+        .then( (data) => {
+            // console.log(data);
+            //res.send(data);
+            res.render("homepage", {
+                todos: data
+            });
+        })
         .catch(error => console.error(error));
 });
 
 app.get("/:id", (req, res) => {
     let id = req.params.id;
     Todo.getOne(id)
-        .then(data => res.send(data))
+        .then((data) => {
+            // res.send(data)
+            res.render("todo-detail-page", data);
+        })
         .catch(error => console.error(error));
 });
 
